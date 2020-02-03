42165252ca2e85ef6c0b7cff4089461c6827a3b0
Samuel Tijani <omalitijani@gmail.com>
2020-02-03 10:19:29 +0100

    Simple binary to decimal conversion file

---
+++ binarytodecimal.py
@@ -0,0 +1,14 @@
+"""
+Author: Samuel Tijani
+File: binarytodecimal.py
+Converts a string of bits to a decimal integer.
+"""
+
+bstring = input("Enter a string of bits: ")
+decimal = 0
+exponent = len(bstring) - 1
+
+for digit in bstring:
+	decimal = decimal + int(digit) * 2 ** exponent
+	exponent -= 1
+print("The integer value is", decimal)
